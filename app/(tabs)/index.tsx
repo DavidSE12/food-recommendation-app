a184a731fa4ea22e505b2e1b4779c165bc26fc6d
Tiendat <tiendat12122004@gmail.com>
2026-01-10 16:42:25 +1100

    Create Footer component

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,13 @@
+import React , {useState, useEffect} from 'react';
+import {View, Text, TextInput} from 'react-native';
+
+
+const Home = () => {
+    return (
+        <View>
+            <Text style = {{color : "black"}}>Hello World</Text>
+        </View>
+    )
+}
+
+export default Home;
